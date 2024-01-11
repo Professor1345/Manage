d2f9311baa300ccd17ff5aca4d376a63005d0d03
Professor1345 <alfulaani1345@gmail.com>
2024-01-12 00:08:11 +0100

    Done with the coding, remain README

--- public/code.js
+++ public/code.js
@@ -1,7 +1,7 @@
 let navBar = document.getElementsByClassName("navbar");
 
 // Background lightColor for UnderMobile
-let mobileUnder = document.getElementsByClassName("mobileBG")
+let mobileUnder = document.getElementsByClassName("mobileBG");
 
 let openButton = () => {
   let open = document.getElementById("open");
@@ -11,11 +11,11 @@ let openButton = () => {
 
   //   Navbar Opening
   navBar[0].style.display = "flex";
-//   Overflow setting
-document.querySelector("body").style.overflow = "hidden";
+  //   Overflow setting
+  document.querySelector("body").style.overflow = "hidden";
 
-// Background lightColor for UnderMobile
-mobileUnder[0].style.display = "block";
+  // Background lightColor for UnderMobile
+  mobileUnder[0].style.display = "block";
 };
 
 let closeButton = () => {
@@ -26,9 +26,33 @@ let closeButton = () => {
 
   //   Navbar Closing
   navBar[0].style.display = "none";
-//   Overflow setting
-document.querySelector("body").style.overflow = "auto";
+  //   Overflow setting
+  document.querySelector("body").style.overflow = "auto";
 
-// Background lightColor for UnderMobile
-mobileUnder[0].style.display = "none";
+  // Background lightColor for UnderMobile
+  mobileUnder[0].style.display = "none";
 };
+
+function submitButton() {
+  //   alert("clickkk");
+  let input = document.getElementById("email").value;
+  document.getElementById("inputError").innerHTML = "";
+
+  let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+// OR let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  try {
+    if (input == "".trim()) throw "The input field is empty.";
+    if(!emailRegex.test(input)) throw "Please insert a valid email";
+    else {
+      return true;
+    }
+    // if (input.t)
+  } catch (error) {
+    // output[0].style.display = "block";
+    document.getElementById("inputError").innerHTML = error;
+    return false;
+  } finally {
+    document.getElementById("email").value = "";
+  }
+}
